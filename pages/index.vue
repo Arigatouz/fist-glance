c8977bcb6b6913f9b1f5ad041087e368846456bc
Arigatou <hictors@gmail.com>
2022-05-14 02:06:44 +0200

    adding product pages and ids

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>let's go to our products <a href="/products">Click Here</a></h1>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ pages/products/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>This is product number : {{ $route.params.id }}</h1>
+    <h2><a :href="products">Products Page</a></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: '/products',
+    }
+  },
+}
+</script>
